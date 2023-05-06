84ffd9168c074a0c70f9a3ef6a32f78787b858c6
tahakoutbi <mehdichbanimehdi@gmail.com>
2023-05-06 18:06:10 +0100

    update project ecomm

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 const HOST = process.env.host;
 
 const app = express()
